c256dc09561e14ec837218085796f5fcdad8b60c
tarohatu2 <tarohatu@gmail.com>
2024-04-10 21:31:54 +0900

    Googleカレンダーっぽいメニューボタン作成

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -2,18 +2,25 @@ import { React } from "react";
 import PropTypes from "prop-types";
 import style from "./AppHeader.module.css";
 import { Link } from "react-router-dom";
-import { MdOutlineSettings, MdLogout } from "react-icons/md";
+import { MdOutlineSettings, MdLogout, MdOutlineFormatListBulleted } from "react-icons/md";
 
 export const AppHeader = (props) => {
   return (
 	<header>
 	  <div className={style.hwrapper}>
 			<div className={style.header}>
-				<h1 className={style.title}>
-					<Link to='/'>
-						{props.title}
-					</Link>
-				</h1>
+				<div className={style.leftcontainer}>
+					<button className={style.listiconbutton}>
+						<MdOutlineFormatListBulleted 
+							className={style.listicon}
+						/>
+					</button>
+					<h1 className={style.title}>
+						<Link to='/'>
+							{props.title}
+						</Link>
+					</h1>
+				</div>
 				<nav>
 					<ul>
 						<li><Link to='settings' className={style.link}><MdOutlineSettings />設定</Link></li>
