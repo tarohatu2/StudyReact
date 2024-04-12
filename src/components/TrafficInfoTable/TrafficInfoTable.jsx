70ffc8ad6e9d051e1c0b6da78055a8ba4056c0e4
tarohatu2 <tarohatu@gmail.com>
2024-04-12 21:40:10 +0900

    テーブル作成

---
+++ src/components/TrafficInfoTable/TrafficInfoTable.jsx
@@ -0,0 +1,32 @@
+import { React } from 'react' 
+import PropTypes from 'prop-types'
+import style from './TrafficInfoTable.module.css'
+
+export const TrafficInfoTable = ({ info }) => { 
+  const rows = info.map((data, index) => {
+    const { distance, direction, reason, title } = data
+    return (
+      <tr id={`jam-${index}`}>
+        <td>{direction}</td>
+        <td>{title}</td>
+        <td>{reason}</td>
+        <td>{distance}</td>
+      </tr>
+    )
+  })
+  return (
+    <table>
+      <tr>
+        <th>方面</th>
+        <th>IC・JCT</th>
+        <th>事由</th>
+        <th>距離</th>
+      </tr>
+      {rows}
+    </table>
+  )
+}
+
+TrafficInfoTable.propTypes = {
+  info: PropTypes.array.isRequired
+}
